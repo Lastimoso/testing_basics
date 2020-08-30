be9adceefd268af611550cf14535e99cf638d7af
Lastimoso <gerardoraiden@gmail.com>
2020-08-30 11:31:37 -0300

    Adds base models

---
+++ db/migrate/20200830142519_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.text :description
+      t.date :due_date
+
+      t.timestamps
+    end
+  end
+end
